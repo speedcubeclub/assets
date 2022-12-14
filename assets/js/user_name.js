3c6cf14cdfbf75db4ceb0261c6b8b4d8f320b841
speedcubeclub <120531943+speedcubeclub@users.noreply.github.com>
2022-12-13 20:59:28 -0800

    Save

--- assets/js/user_name.js
+++ assets/js/user_name.js
@@ -1,4 +1,4 @@
-function User_Name() {
+function User_Name_UP() {
     let User_Name_dom = document.getElementById("User_Name");
     User_Name_dom.innerHTML = sessionStorage.getItem("User_Name");
 }
