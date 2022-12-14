debe2bca17e29826c4d97687712609b221c85494
speedcubeclub <120531943+speedcubeclub@users.noreply.github.com>
2022-12-13 18:40:04 -0800

    Save

--- rank/assets/js/rank.js
+++ rank/assets/js/rank.js
@@ -1,25 +1,27 @@
 function rank_fill() {
     let Rank_Block = document.getElementById("Rank_Block");
-
+    let Table = document.createElement("table");
+    Table.className = "Table";
     for (let i = 0; i < Rank_Database.length; i++) {
-        let New_Person = document.createElement("div");
+        let New_Person = document.createElement("tr");
         New_Person.className = "New_Person";
-    
-        let New_Person_Time = document.createElement("p");
+
+        let New_Person_Time = document.createElement("td");
         New_Person_Time.className = "New_Person_Time";
-        New_Person_Time.innerHTML= Rank_Database[i][0];
+        New_Person_Time.innerHTML = Rank_Database[i][0];
 
-        let New_Person_Name = document.createElement("p");
+        let New_Person_Name = document.createElement("td");
         New_Person_Name.className = "New_Person_Name";
-        New_Person_Name.innerHTML= Rank_Database[i][1];
+        New_Person_Name.innerHTML = Rank_Database[i][1];
 
-        let New_Person_Day = document.createElement("p");
+        let New_Person_Day = document.createElement("td");
         New_Person_Day.className = "New_Person_Day";
-        New_Person_Day.innerHTML= Rank_Database[i][2];
+        New_Person_Day.innerHTML = Rank_Database[i][2];
 
         New_Person.append(New_Person_Time);
         New_Person.append(New_Person_Name);
         New_Person.append(New_Person_Day);
-        Rank_Block.append(New_Person);
+        Table.append(New_Person);
     }
+    Rank_Block.append(Table);
 }
