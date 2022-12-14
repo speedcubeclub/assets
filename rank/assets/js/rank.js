ffcf3ada4a1e26df2aa185a949d47fb1185450bf
speedcubeclub <120531943+speedcubeclub@users.noreply.github.com>
2022-12-13 18:17:15 -0800

    Save

--- rank/assets/js/rank.js
+++ rank/assets/js/rank.js
@@ -1,4 +1,11 @@
 function rank_fill() {
     let Rank_Block = document.getElementById("Rank_Block");
 
+    for (let i = 0; i < Rank_Database.length; i++) {
+        let New_Person = document.createElement("div");
+        New_Person.className = "New_Person";
+    
+        let New_Person_Time = document.createElement("div");
+        New_Person_Time.className = "New_Person_Time";
+    }
 }
