2231cbadd922bfc0021eb0009e7e63d0db2139dd
speedcubeclub <120531943+speedcubeclub@users.noreply.github.com>
2022-12-13 17:37:09 -0800

    Save

---
+++ login/assets/js/user_repository.js
@@ -0,0 +1,4 @@
+let User_Library =
+    [
+        ["TonyKan", "Ktc000488", "kantiancheng0917@gmail.com"],
+    ];
---
+++ rank/assets/js/rank.js
@@ -0,0 +1,4 @@
+function rank_fill() {
+    let Rank_Block = document.getElementById("Rank_Block");
+
+}
