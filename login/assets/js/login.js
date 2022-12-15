917d1e32b5f58ca596526cdcecabac364be39089
speedcubeclub <120531943+speedcubeclub@users.noreply.github.com>
2022-12-14 19:27:50 -0800

    Save

--- login/assets/js/login.js
+++ login/assets/js/login.js
@@ -6,6 +6,8 @@ function login() {
     let Username = document.getElementById("Username").value;
     let Password = document.getElementById("Password").value;
     for (let i = 0; i < User_Library.length; i++) {
+        console.log("开始对比用户名！");
+        console.log(User_Library[i][0].toLowerCase() + '==' + Username.toLowerCase() + "||" + User_Library[i][2] + "==" + Username);
         if (User_Library[i][0].toLowerCase() == Username.toLowerCase() || User_Library[i][2] == Username) {
             console.log(User_Library[i][0].toLowerCase() + "==" + Username.toLowerCase());
             console.log("用户名正确！");
