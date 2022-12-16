32ed7b5cd9fb094fbae9ea2619cc7032a3657ab0
speedcubeclub <120531943+speedcubeclub@users.noreply.github.com>
2022-12-15 17:21:23 -0800

    Save

--- record/assets/js/record.js
+++ record/assets/js/record.js
@@ -4,4 +4,7 @@ function record_fill() {
 [姓名, 时间1, 时间2, 时间3, 最快]]
 */
     let Record_Block = document.getElementById("Record_Block");
+    for (let i = 0; i < Record_Database.length; i++){
+        
+    }
 }
