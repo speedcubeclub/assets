3f3c83a0edbe94743249c75b91b0bc15399a8bc3
speedcubeclub <120531943+speedcubeclub@users.noreply.github.com>
2023-01-18 14:21:08 -0800

    Save

--- assets/js/login_check.js
+++
@@ -1,9 +0,0 @@
-let User_Name = sessionStorage.getItem("User_Name");
-
-if (User_Name == "" || User_Name == null) {
-    //未登录
-    window.location = "./login/";
-} else if (User_Name != "" && User_Name != null) {
-    //登录
-    console.log("Successful automatic login!");
-} 
--- assets/js/user_name.js
+++
@@ -1,4 +0,0 @@
-function User_Name_UP() {
-    let User_Name_dom = document.getElementById("User_Name");
-    User_Name_dom.innerHTML = sessionStorage.getItem("User_Name");
-}
